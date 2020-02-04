3b1cd3b12786fcbb4d5367e97c2db65d787008f5
Clinton Osaghae Harrison <P2436430@my365.dmu.ac.uk>
2020-02-04 10:32:23 +0000

    Added testing and class for the stock management

---
+++ TrainerTesting/tstStock.cs
@@ -0,0 +1,22 @@
+﻿using TrainerClasses;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TrainerTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsStock Stock = new clsStock();
+
+            Assert.IsNotNull(Stock);
+        }
+    }
+}
+
+
+
