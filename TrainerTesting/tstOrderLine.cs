6daad194289348724db93ed8a8a629d30380d851
Troy <P2508213@my365.dmu.ac.uk>
2020-02-11 10:36:11 +0000

    Started up to the web presentation layer, all tests are currently working

--- TrainerTesting/tstOrderLine.cs
+++ TrainerTesting/tstOrderLine.cs
@@ -1,17 +1,54 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TrainerClasses;
 
 namespace TrainerTesting
 {
+    [TestClass]
     public class tstOrderline
     {
-       [TestMethod]
-            public void OrderLineExists()
-            {
-                clsOrderline AnOrderL = new clsOrderline();
-                Assert.IsNotNull(AnOrderL);
-            }
+        [TestMethod]
+        public void OrderLineExists()
+        {
+            clsOrderline AnOrderL = new clsOrderline();
+            Assert.IsNotNull(AnOrderL);
+        }
+
+        [TestMethod]
+        public void QuantityOK()
+        {
+            clsOrderline AnOrderL = new clsOrderline();
+            int TestData = 3;
+            AnOrderL.Active = TestData;
+            Assert.AreEqual(AnOrderL.Active, TestData);
+
+        }
+        [TestMethod]
+        public void OLineNumberOK()
+        {
+            clsOrderline AnOrderL = new clsOrderline();
+            int TestData = 1;
+            AnOrderL.Active = TestData;
+            Assert.AreEqual(AnOrderL.Active, TestData);
+
+        }
+        [TestMethod]
+        public void ONumberOK()
+        {
+            clsOrderline AnOrderL = new clsOrderline();
+            int TestData = 5;
+            AnOrderL.Active = TestData;
+            Assert.AreEqual(AnOrderL.Active, TestData);
+
+        }
+        [TestMethod]
+        public void ShoeIDOK()
+        {
+            clsOrderline AnOrderL = new clsOrderline();
+            int TestData = 2;
+            AnOrderL.Active = TestData;
+            Assert.AreEqual(AnOrderL.Active, TestData);
+
         }
     }
-
-
+}
