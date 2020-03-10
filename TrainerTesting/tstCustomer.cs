250dfdba3877478b41fa0b95a20405e3cc55323a
Tony Bakasi <P2507596@my365.dmu.ac.uk>
2020-03-10 10:22:43 +0000

    Added testing and class for Customer and Staff management

---
+++ TrainerTesting/tstCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainerClasses;
+
+namespace TrainerTesting
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+            //checks to see if an instance can be created of clsCustomer
+        {
+            //create an object based on clsCustomer
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+
+        }
+    }
+}
