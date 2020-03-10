c8594916ab8db9bcf6bd00321826227df2c43a25
Troy <P2508213@my365.dmu.ac.uk>
2020-03-10 10:40:16 +0000

    Up to find

---
+++ TrainerTesting/tstCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrainerTesting
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
