604fe8dc0f889475dbf3907ef125eb7d1787184f
Troy Briscoe <tbriscoe742@gmail.com>
2020-05-13 19:19:43 +0100

    Test Order + Orderline complete

--- TrainerClasses/clsOrder.cs
+++ TrainerClasses/clsOrder.cs
@@ -2,7 +2,7 @@
 
 namespace TrainerClasses
 {
-    public class clsOrder
+    public class ClsOrder
     {
         private DateTime mDate;
         private Int32 mCustomerID;
@@ -65,8 +65,27 @@ namespace TrainerClasses
                 mComplete = value;
             }
         }
+          public int Active { get; set; }
+        public bool Find(int ONumber)
+        {
+            ClsDataConnection DB = new ClsDataConnection();
+            DB.AddParameter("@ONumber", ONumber);
+            DB.Execute("sproc_tblOrder_FilterByONumber");
 
+            if (DB.Count == 1)
+            {
+                mONumber = Convert.ToInt32(DB.DataTable.Rows[0]["ONumber"]);
+                mCustomerID = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerID"]);
+                mComplete = Convert.ToBoolean(DB.DataTable.Rows[0]["OrderComplete"]);
+                mNotes = Convert.ToString(DB.DataTable.Rows[0]["AdditionalNotes"]);
+                mDate = Convert.ToDateTime(DB.DataTable.Rows[0]["DateofEntry"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 
-        public int Active { get; set; }
+        }
     }
 }
--- TrainerTesting/tstOrder.cs
+++ TrainerTesting/tstOrder.cs
@@ -10,14 +10,14 @@ namespace TrainerTesting
         [TestMethod]
         public void OrderExists()
         {
-            clsOrder AnOrder = new clsOrder();
+            ClsOrder AnOrder = new ClsOrder();
             Assert.IsNotNull(AnOrder);
         }
 
         [TestMethod]
         public void CustomerIDOK()
         {
-            clsOrder AnOrder = new clsOrder();
+            ClsOrder AnOrder = new ClsOrder();
             int TestData = 1;
             AnOrder.CustomerID = TestData;
             Assert.AreEqual(AnOrder.CustomerID, TestData);
@@ -26,7 +26,7 @@ namespace TrainerTesting
         [TestMethod]
         public void ONumberOK()
         {
-            clsOrder AnOrder = new clsOrder();
+            ClsOrder AnOrder = new ClsOrder();
             int TestData = 1;
             AnOrder.ONumber = TestData;
             Assert.AreEqual(AnOrder.ONumber, TestData);
@@ -35,7 +35,7 @@ namespace TrainerTesting
         [TestMethod]
         public void NotesOK()
         {
-            clsOrder AnOrder = new clsOrder();
+            ClsOrder AnOrder = new ClsOrder();
             string TestData = "Reception";
             AnOrder.Notes = TestData;
             Assert.AreEqual(AnOrder.Notes, TestData);
@@ -44,7 +44,7 @@ namespace TrainerTesting
         [TestMethod]
         public void CompleteOK()
         {
-            clsOrder AnOrder = new clsOrder();
+            ClsOrder AnOrder = new ClsOrder();
             bool TestData = true;
             AnOrder.Complete = TestData;
             Assert.AreEqual(AnOrder.Complete, TestData);
@@ -53,12 +53,99 @@ namespace TrainerTesting
         [TestMethod]
         public void DateOK()
         {
-            clsOrder AnOrder = new clsOrder();
+            ClsOrder AnOrder = new ClsOrder();
             DateTime TestData = DateTime.Now.Date;
             AnOrder.Date = TestData;
             Assert.AreEqual(AnOrder.Date, TestData);
 
         }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            ClsOrder AnOrder = new ClsOrder();
+            Boolean Found = false;
+            Int32 ONumber = 3;
+            Found = AnOrder.Find(ONumber);
+
+            Assert.IsTrue(Found);
+        }
+        [TestMethod]
+        public void FindONumberOK()
+        {
+            ClsOrder AnOrder = new ClsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 ONumber = 1;
+            Found = AnOrder.Find(ONumber);
+
+            if (AnOrder.ONumber != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void FindCustomerIDOK()
+        {
+            ClsOrder AnOrder = new ClsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 ONumber = 1;
+            Found = AnOrder.Find(ONumber);
+
+            if (AnOrder.CustomerID != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void FindNotesOK()
+        {
+            ClsOrder AnOrder = new ClsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 ONumber = 1;
+            Found = AnOrder.Find(ONumber);
+
+            if (AnOrder.Notes != "Leave with reception")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void FindCompleteOK()
+        {
+            ClsOrder AnOrder = new ClsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 ONumber = 1;
+            Found = AnOrder.Find(ONumber);
+
+            if (AnOrder.Complete != false)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void FindDateOK()
+        {
+            ClsOrder AnOrder = new ClsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 ONumber = 1;
+            Found = AnOrder.Find(ONumber);
+
+            if (AnOrder.Date != Convert.ToDateTime("11/01/2020"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+
 
     }
 }
