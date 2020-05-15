1a309dfb4782853ebaba934d6e78e394a279eb53
Clinton Osaghae Harrison <P2436430@my365.dmu.ac.uk>
2020-05-15 11:16:55 +0100

    Finsihed the test class started maing the form. The form is nearly completed.Will be done tomrrow

--- TrainerClasses/clsStock.cs
+++ TrainerClasses/clsStock.cs
@@ -1,9 +1,18 @@
-﻿namespace TrainerClasses
+﻿using System;
+
+namespace TrainerClasses
 {
     public class clsStock
     {
         public clsStock()
         {
         }
+
+        public int Shoeid { get; set; }
+        public string Name { get; set; }
+        public DateTime Release { get; set; }
+        public int QuantityStock { get; set; }
+        public int PriceStock { get; set; }
+        public int Size { get; set; }
     }
 }
